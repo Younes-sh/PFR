83fe260b97f3c166b5fd6ab87e7219e4c07a1ffa
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-13 19:10:02 +0100

    create assets

--- portfolio/components/Navbar/Navbar.jsx
+++ portfolio/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
-
-
+import '../../styles/globals.css'
 
 const Navbar = () => {
+    
   return (
     <div className='w-full h-16 bg-cyan-700 flex justify-center items-center'>
-        <ul className='w-2/3 h-full flex bg-red-200'>
+        <ul className='w-2/3 h-full flex '>
             <li className=' m-4'>
                 <Link rel="stylesheet" href="" >Home</Link>
             </li>
