fd2ddd1f1787754fd080a4d6f6e6d29b15884992
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-13 17:59:35 +0100

    styed Navbar component

--- portfolio/components/Navbar/Navbar.jsx
+++ portfolio/components/Navbar/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div className='w-full h-16 bg-cyan-700'>
-        <ul>
-            <li>
+    <div className='w-full h-16 bg-cyan-700 flex justify-center items-center'>
+        <ul className='w-2/3 h-full flex bg-red-200'>
+            <li className=' m-4'>
                 <Link rel="stylesheet" href="" >Home</Link>
             </li>
-            <li>
+            <li className=' m-4'>
                 <Link rel="stylesheet" href="" >Contact</Link>
             </li>
-            <li>
+            <li className=' m-4'>
                 <Link rel="stylesheet" href="" >Projects</Link>
             </li>
             
