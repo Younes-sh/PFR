800b72e0863a935707a9ceb747150db34df83cb1
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-13 23:28:13 +0100

    remove useRef

--- portfolio/components/Navbar/Navbar.jsx
+++ portfolio/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import {useRef} from 'react'
 import Link from 'next/link'
 import '../../styles/globals.css'
+
+
+
 
 const Navbar = () => {
     
   return (
     <div className='w-full h-16 bg-cyan-700 flex justify-center items-center'>
         <ul className='w-2/3 h-full flex '>
-            <li className=' m-4'>
+            <li  className=' m-4'>
                 <Link rel="stylesheet" href="" >Home</Link>
             </li>
             <li className=' m-4'>
