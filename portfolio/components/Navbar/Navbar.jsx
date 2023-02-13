de45d41093351a1cc8eee7854682bfafe25e0ba1
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-13 16:53:33 +0100

    create of Navbar component

---
+++ portfolio/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <Link rel="stylesheet" href="" >Home</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
