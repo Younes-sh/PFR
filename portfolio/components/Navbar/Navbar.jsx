230e7c8f2e900d4bfcc3c67b5e0f86c99551d62f
younes <98320752+Younes-sh@users.noreply.github.com>
2023-02-14 11:43:18 +0100

    remove tailwind css

--- portfolio/components/Navbar/Navbar.jsx
+++ portfolio/components/Navbar/Navbar.jsx
@@ -8,15 +8,15 @@ import '../../styles/globals.css'
 const Navbar = () => {
     
   return (
-    <div className='w-full h-16 bg-cyan-700 flex justify-center items-center'>
-        <ul className='w-2/3 h-full flex '>
-            <li  className=' m-4'>
+    <div className=''>
+        <ul className=''>
+            <li  className=' '>
                 <Link rel="stylesheet" href="" >Home</Link>
             </li>
-            <li className=' m-4'>
+            <li className=' '>
                 <Link rel="stylesheet" href="" >Contact</Link>
             </li>
-            <li className=' m-4'>
+            <li className=' '>
                 <Link rel="stylesheet" href="" >Projects</Link>
             </li>
             
